b853cb6fd2b4a4845360f59acf688f1b9bb90d5e
Vinosandy <rvinothini@yahoo.co.in>
2024-01-13 16:10:25 +0000

    File structure for the project created

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+//
+
+var displayQuestions=[
+       
+{
+    questions:"How do you find the minimum of x and y using JavaScript? ",
+    options:["min(x,y)","Math.min(x,y)","Math.min(xy)"," min(xy)"],
+    answer:"Math.min(xy)"
+},
+{
+    questions:"Which JavaScript label catches all the values, except for the ones specified?",
+    options:["Catch","Label","Try","Default"],
+    answer:"Default"
+},
+{
+    questions:"Which statement cannot be used to declare a variable in JavaScript?",
+    options:["Let","var","const","int"],
+    answer:"int"
+},
+{
+    questions:"Which of the following is an array method?",
+    options:["Map","Filter","Reduce","All of these"],
+    answer:"All of these"
+},
+{
+    questions:"What will ‘0 == false ’ evaluate to?",
+    options:["True","False","null","throws error"],
+    answer:"True"
+},
+
+];
+
