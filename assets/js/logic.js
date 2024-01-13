9915fba49ef29a3b59bae81ff609a4ff58829ba1
Vinosandy <rvinothini@yahoo.co.in>
2024-01-13 17:54:34 +0000

    function to set timer created in logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,9 @@
+//import displayQuestions from "./questions"
+
 //Getting DOM elements from index.html
 
 var timerEl= document.getElementById("#time");
+var startScreen=document.getElementById("#start-screen");
 var startBtn= document.getElementById("#start");
 var questionsEl= document.getElementById("#questions");
 var choiceEl = document.getElementById("#choices");
@@ -8,3 +11,32 @@ var initialEl= document.getElementById("#initials");
 var submitBtn = document.getElementById("#submit");
 var feedbackEl= document.getElementById("feedback");
 
+// Initial status of time and qusetion before starting the quiz
+var currentQuestionIndex = 0;
+var timer;
+var time= displayQuestions.length*10;
+
+console.log("Timer:"+time);
+
+//Function to start timer
+
+function startTimer(){
+
+timer= setInterval(function clock(){
+    time--;
+    timerEl.textContent=time;
+    if(time<=0){
+        return ;
+    }
+},1000);
+
+timer.textContent=time;
+console.log("Time inside interval:"+time);
+
+//dynamically hiding the start screen
+startScreen.setAttribute("class","hide");
+
+//dynamically making the question element visible
+questionsEl.removeAttribute("class");
+
+}
