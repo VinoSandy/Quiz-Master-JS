2339d5a946e0c42ae6a090a1b78dfd99125c8579
Vinosandy <rvinothini@yahoo.co.in>
2024-01-13 16:45:34 +0000

    logic.js is updated by getting DOM elements and defining variables

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+//Getting DOM elements from index.html
+
+var timerEl= document.getElementById("#time");
+var startBtn= document.getElementById("#start");
+var questionsEl= document.getElementById("#questions");
+var choiceEl = document.getElementById("#choices");
+var initialEl= document.getElementById("#initials");
+var submitBtn = document.getElementById("#submit");
+var feedbackEl= document.getElementById("feedback");
+
